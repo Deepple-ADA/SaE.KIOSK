174abdc79fef4e987ca6c2068873a92f89b6c97d
Greed <suby0475@gmail.com>
2023-08-24 15:01:47 +0900

    Color Extension, make mainView

---
+++ hackerton/Extension/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  hackerton
+//
+//  Created by Greed on 2023/08/24.
+//
+
+import Foundation
---
+++ hackerton/View/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  hackerton
+//
+//  Created by Cho Chaewon on 2023/08/17.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 32) {
+            RoundedRectangle(cornerRadius: 32)
+                .frame(width: 960, height: 635)
+                .foregroundColor(.secondary)
+                .overlay(
+                    Image("adExample")
+                )
+            
+            HStack(spacing: 30){
+                VStack(spacing: 53) {
+                    Image("smileFace")
+                    VStack{
+                        Text("음성으로 주문하려면")
+                            .foregroundColor(.Textprimary)
+                        Text("화면을 바라보고")
+                            .foregroundColor(.PointPrimary)
+                        HStack(spacing: 0){
+                            Text("말씀")
+                                .foregroundColor(.PointPrimary)
+                            Text("해주세요")
+                                .foregroundColor(.Textprimary)
+                        }
+                    }
+                }
+                .font(Font.custom("Inter", size: 36))
+                
+                .frame(width: 464, height: 604)
+                VStack {
+                    Image("orderBtn")
+                }
+                .frame(width: 464, height: 604)
+            }
+        }
+        .foregroundColor(.gray)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
